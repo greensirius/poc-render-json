ce57bed88e9724468818a3a512f7de9d3eccb09c
Simon <sstur@me.com>
2019-03-18 16:40:34 -0700

    Add Expo

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
 import React, { Component } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <View style={styles.container}>
+        <Text>
+          Edit src/App.js and save to reload.
+        </Text>
+      </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
 
 export default App;
