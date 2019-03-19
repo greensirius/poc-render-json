87efa2c8852952a891b046eb44427f1b71a8e39a
Simon <sstur@me.com>
2019-03-18 21:49:49 -0700

    Use TypeScript, TSLint, Prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
-import React, { Component } from 'react';
+import * as React from 'react';
+import { Component } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 
 class App extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>
-          Edit src/App.js and save to reload.
-        </Text>
+        <Text>Edit src/App.js and save to reload.</Text>
       </View>
     );
   }
---
+++ src/__tests__/App.test.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import App from '../App';
+
+import renderer from 'react-test-renderer';
+
+jest.useFakeTimers();
+
+it('renders without crashing', () => {
+  const rendered = renderer.create(<App />).toJSON();
+  expect(rendered).toBeTruthy();
+});
